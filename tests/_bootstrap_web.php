0e8932bf0dcaf771189fd51638711608c891005e
SonicGD <sonicgd@gmail.com>
2015-06-05 09:25:33 +0500

    add bootstrap for web tests

---
+++ tests/_bootstrap_web.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sonic
+ * Date: 6/5/2015
+ * Time: 9:14 AM
+ */
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+
+// fcgi doesn't have STDIN and STDOUT defined by default
+defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
+defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/../config/web.php');
+
+$application = new yii\web\Application($config);
